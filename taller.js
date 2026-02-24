0ff6569d46fb0e3530037fdc62215114d8950c2a
yuleidimonteroobando <yuleidimontero28@gmail.com>
2026-02-24 08:33:44 +0200

    subo mi primer commit creando(mis respuestas.md y mi taller.js

---
+++ taller.js
@@ -0,0 +1,5 @@
+let nombre = "maria"
+let edad = 25
+let ciudad = "bogota"
+
+console.log("hola esta informacion es tuya? tu nombre es?" + nombre + ", tu edad es? " + edad + ",y vives en? " + ciudad)
